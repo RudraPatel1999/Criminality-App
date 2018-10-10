dc8d2a961654461e634d8b590cea8716b3a2cb0d
RudraPatel1999 <patelrudra7399@gmail.com>
2018-10-10 14:22:45 +0530

    Add Project

---
+++ CriminalityApp/app/src/main/java/com/example/rudra/criminalityapp/SliderScreen.java
@@ -0,0 +1,14 @@
+package com.example.rudra.criminalityapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SliderScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slider_screen);
+
+    }
+}
